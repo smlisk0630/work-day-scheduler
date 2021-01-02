2473b0b8b6ac077f943e2665cd5f42d6e2e4b205
slisk <63676970+smlisk0630@users.noreply.github.com>
2021-01-02 17:03:32 -0500

    Added date to jumbotron in JS file

---
+++ Assets/script.js
@@ -0,0 +1,18 @@
+// 1. Display current day at top of app
+    // Install Day.js library
+    // Create date section
+    // Create loop
+    // Create timeblock section
+    // Get timeblock element
+    // Create new section and add date/time text content
+    // Add new section and content to parent section
+
+
+$(document).ready(function() {
+    var jumbotron = document.getElementsByClassName("jumbotron");
+    var getDate = dayjs().format('MMMM-DD-YYYY');
+    var dateBlock = $("<p>" + getDate +  "</p>");
+    $(jumbotron).append(dateBlock);
+    // var newTimeBlock = $("#time-block");
+    // container.append(newTimeBlock);
+});
