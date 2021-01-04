7de82e5829a1d210d54162631385332d924b9e72
slisk <63676970+smlisk0630@users.noreply.github.com>
2021-01-04 18:28:06 -0500

    Fixed logic issue preventing color change. Also added input width.

--- Assets/script.js
+++ Assets/script.js
@@ -1,53 +1,38 @@
-// 1. Display current day at top of app
-    // Install Day.js library
-    // Create date section
-    // Create loop
-    // Create timeblock section
-    // Get timeblock element
-    // Create new section and add date/time text content
-    // Add new section and content to parent section
-var businessHourList = [
-    "9AM",
-    "10AM",
-    "11AM",
-    "12PM",
-    "1PM",
-    "2PM",
-    "3PM",
-    "4PM",
-    "5PM"
-];
+function time() {
+
+var businessDate = new Date();
+
+// 
+var businessHour = dayjs().hour();
+console.log(businessHour);
+
 
 $(document).ready(function() {
     // get jumbotron section
     var jumbotron = document.getElementsByClassName("jumbotron");
-    // call day.js and format date as month-day-year
+    // call current date/time and format as month-day-year
     var getDate = dayjs().format('MMMM DD, YYYY');
     //create new paragraph and add date to it
     var dateBlock = $("<p>" + getDate +  "</p>");
     // append new paragraph and date to jumbotron section
     $(jumbotron).append(dateBlock);
 
-    // get time-block section
-    var timeBlock = document.getElementById("#time-block");
-
-    // loop through the business hours array and append to time-block section
-    $.each(businessHourList,function(i,hour) {
-        var newTimeBlock = $("<p>" + hour + "</p>");
-        $("#time-block").append(newTimeBlock);
-        // add event listener and append to time-block section
-        $("#time-block").click(function(){
-            var newEvent = $("<input>").attr({type: "text"}
-            );
-            newTimeBlock.append(newEvent);
-        });
         // change colors based on current time
-        // if(businessHourList < now) {
-        //     newEvent.attr("gray");
-        // } else if(businessHourList === now) {
-        //     newEvent.attr("red");
-        // } else {
-        //     newEvent.attr("green");
-        // }
-    });
-});
+        $(".row input").each(function()  {
+            // add data-time attribute and convert to hour format using dayjs
+            // convert string to number
+            var currentTime = parseInt($(this).attr("data-time"));
+            // change colors based on currentTime
+            if(currentTime < businessHour) {
+                $(this).parent().addClass("past");
+            } else if(currentTime === businessHour) {
+                $(this).parent().addClass("present");
+            } else {
+                $(this).parent().addClass("future");
+            }
+            console.log(currentTime);
+        })
+    })
+}
+
+time();
